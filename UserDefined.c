f25dc6bda2e7141562f29d4af3732cc5b2db86e5
MintVM_JC <A01365969@itesm.mx>
2018-02-04 20:20:30 -0600

    Defined PrintItem & PrintList

--- UserDefined.c
+++ UserDefined.c
@@ -1,5 +1,28 @@
+#include <stdio.h>                                    // Used for printf
 #include <stdlib.h>                     // Used for malloc, & EXIT codes
 #include "UserDefined.h"               // All the user defined functions
+
+int PrintItem (const void *data_p){
+  node_p p = (node_p)data_p;
+  if(p != NULL){
+    printf("Data element %d: %s\n", p->number, p->theString);
+    return EXIT_SUCCESS;
+  }else{
+    return EXIT_FAILURE;
+  }
+}
+
+int PrintList (GList * myList_p){
+  GList *l;
+  int result = EXIT_FAILURE;
+  for(l = myList_p; l != NULL; l = l->next){
+    result = PrintItem(l->data);
+    if(result == EXIT_FAILURE){
+      break;
+    }
+  }
+  return result;
+}
 
 node_p NewItem (int theNumber, char * theString){
   node_p newItem = (node_p)malloc(sizeof(myData));
